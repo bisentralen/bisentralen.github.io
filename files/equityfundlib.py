69240818aaea582f45a4adc4b4a75ba0f811a608
Fredrik H. Juell <fjuell@users.noreply.github.com>
2023-04-20 14:28:40 +0200

    Trimmed versions

--- files/equityfundlib.py
+++ files/equityfundlib.py
@@ -14,14 +14,6 @@ class Fund:
 		self._portfolio = {}	# Dict of PortfolioEntry-objects with ticker as key
 
 
-	def __str__(self):
-		"""Overriding the built-in string representation of a fund object. Returns key fund data as a formatted string."""
-		cash, assets, total = self.get_value()
-		cash = "{:.2f}".format(cash); assets = "{:.2f}".format(assets); total = "{:.2f}".format(total)
-		fund_summary = f"Name: {self._name}\nOwner: {Fund.owner_name}\nCash value: {str(cash)}\nAssets value: {str(assets)}\nTotal value: {str(total)}"
-		return fund_summary
-
-
 	def portfolio_as_dataframe(self):
 		"""Returns the portfolio as a Pandas DataFrame."""
 		df_portfolio = pd.DataFrame(columns=["ticker", "company_name", "price_bought", "price_now", "volume"])
@@ -46,36 +38,20 @@ class Fund:
 		self._cash = self._cash - volume*price
 
 
-	def sell_stock(self, stock, volume="all"):
-		"""Takes a stock object and optional volume."""
-		ticker, c_name, price = stock.to_list()
-		if volume == "all":
-			volume = self._portfolio[ticker].volume
-			# Update cash holding
-			self._cash = self._cash + volume*price
-			# Remove entry from portfolio
-			self._portfolio.pop(ticker)
-		elif 0 <= volume <= self._portfolio[ticker].volume:
-			# Update volume
-			self._portfolio[ticker].volume -= volume
-			# Update cash holding
-			self._cash = self._cash + volume*price
-		elif volume > self._portfolio[ticker].volume:
-			print("Error: You cannot sell more than you have. This is not a hedge fund.")
-
-
 	def update_price(self, ticker, price_new):
 		"""Takes a ticker and prices, and updates the price of this stocks in the portfolio."""
 		self._portfolio[ticker].price_now = price_new
 
 
-	def get_value(self):
+	def get_value(self, nice=False):
 		"""Returns a list with three elements: Cash Value, Assets Value, Total Value."""
 		cash_value = self._cash
 		assets_value = 0
 		for ticker, entry in self._portfolio.items():
 			assets_value += entry.price_now*entry.volume
 		total_value = cash_value + assets_value
+		if nice:
+			return f"Cash Value: {cash_value}\nAssets Value: {assets_value}\nTotal Value: {total_value}"
 		return [cash_value,assets_value,total_value]
 
 
--- files/main.py
+++ files/main.py
@@ -3,16 +3,6 @@ import matplotlib.pyplot as plt
 import equityfundlib as ef
 
 
-def load_stocks(stocks_file):
-	"""Takes a csv file as input, and returns a dict of stock objects."""
-	df = pd.read_csv(stocks_file)	# A Pandas DataFrame
-	stocks = {}						# An empty dictionary
-	for index, row in df.iterrows():
-		# Loop over DataFrame, make a Stock object for each line, and save it in a dict
-		stocks[row['ticker']] = ef.Stock(row['ticker'], row['company_name'], row['price'])
-	return stocks 	# Returning a dict of stocks with ticker as key and Stock object as value
-
-
 # update stock prices
 def update_stock_prices():
 	# Update stock objects
@@ -23,20 +13,26 @@ def update_stock_prices():
 		my_fund.update_price(ticker, stocks[ticker].get_price())
 
 
-# Make a dictionary with stock objects with, by calling function load_stocks
-stocks = load_stocks("https://bisentralen.github.io/files/data_stocks.csv")
-#stocks = load_stocks("https://tinyurl.com/bistocks")
+
+# Read CSV file into a Pandas DataFrame
+df = pd.read_csv("https://bisentralen.github.io/files/data_stocks.csv")	# A Pandas DataFrame
+stocks = {}		# An empty dictionary
+for index, row in df.iterrows():
+	# Loop over DataFrame, make a Stock object for each line, and save it in a dict
+	stocks[row['ticker']] = ef.Stock(row['ticker'], row['company_name'], row['price'])
+
+
 
 # Make a fund
 my_fund = ef.Fund("More is More")
 
 # Buy stocks
-my_fund.buy_stock(stocks['bbb'], 500)
+my_fund.buy_stock(stocks['bbb'], 150)
 my_fund.buy_stock(stocks['nid'], 1000)
-my_fund.buy_stock(stocks['nys'], 2500)
+my_fund.buy_stock(stocks['nys'], 1500)
 
 # Print fund data
-print(my_fund)
+print(my_fund.get_value("nice"))
 print(my_fund.portfolio_as_dataframe())
 print("---------------------------------------------------------------------")
 
@@ -44,33 +40,20 @@ print("---------------------------------------------------------------------")
 update_stock_prices()
 
 # Print fund data
-print(my_fund)
+print(my_fund.get_value("nice"))
 print(my_fund.portfolio_as_dataframe())
 print("---------------------------------------------------------------------")
 
 # Buy more
-my_fund.buy_stock(stocks['nid'], 500)
+my_fund.buy_stock(stocks['nid'], 300)
 my_fund.buy_stock(stocks['alu'], 400)
 
 update_stock_prices()
 
 # Print fund data
-print(my_fund)
+print(my_fund.get_value("nice"))
 print(my_fund.portfolio_as_dataframe())
 print("---------------------------------------------------------------------")
-
-
-# Sell stocks
-my_fund.sell_stock(stocks['bbb'])
-my_fund.sell_stock(stocks['nys'], 2000)
-
-
-# Print fund data
-print(my_fund)
-print(my_fund.portfolio_as_dataframe())
-print("---------------------------------------------------------------------")
-
-
 
 
 
@@ -79,18 +62,15 @@ print("---------------------------------------------------------------------")
 #####################################################
 # Plot a time series of the total value of the fund
 
-# Store the portfolio in a variable, for easier reading
-portfolio = my_fund.get_portfolio()
-
 # Make a list of ascending integers to represent time
-x2 = [i for i in range(0,800)]
+x = [i for i in range(0,1000)]
 
 # Make an empty list to store fund values for each point in time
-y_value = []
+y = []
 
 # For each time, store the value and update prices
-for time in x2:
-	y_value.append(my_fund.get_value()[2])
+for time in x:
+	y.append(my_fund.get_value()[2])
 	update_stock_prices()
 
 # Make a moving average for value
@@ -99,16 +79,16 @@ ma_length = 50
 # Fill the first ma_length list elements with empty value that is not plotted
 for i in range(ma_length-1): 
 	ma.append(float("nan"))
-# Calculate moving average for each y_value, and put it in the ma list
-for i in range(ma_length-1, len(y_value)):
-	ma.append(sum(y_value[(i-(ma_length-1)):(i+1)])/ma_length)
+# Calculate moving average for each y, and put it in the ma list
+for i in range(ma_length-1, len(y)):
+	ma.append(sum(y[(i-(ma_length-1)):(i+1)])/ma_length)
 
 
 
 # Make the value plot
-plt.plot(x2, y_value, label="Total Value")
+plt.plot(x, y, label="Total Value")
 # Make the moving average plot
-plt.plot(x2, ma, label="MA"+str(ma_length)) 
+plt.plot(x, ma, label="MA"+str(ma_length)) 
 # Descriptive elements
 plt.xlabel("Time")
 plt.ylabel("Value")
